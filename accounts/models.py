38ce93da1b4a3a9dc30fc0323925df6dc59c4925
amen <fikruamen11@gmail.com>
2025-02-28 13:43:13 +0300

    added a user attribute to customer model

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 from .models import *
 
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=20, null=True)
     email = models.CharField(max_length=30, null=True)
