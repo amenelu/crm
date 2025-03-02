7c9e6cb97c49dcb8ba41ee673de92dcfd3788a86
amen <fikruamen11@gmail.com>
2025-03-02 14:36:21 +0300

    only pending counter is working

--- accounts/views.py
+++ accounts/views.py
@@ -74,9 +74,18 @@ def home(request):
 @login_required(login_url="login")
 @allowed_users(allowed_roles=["customer"])
 def userPage(request):
-    customer = getattr(request.user, "customer", None)
-    orders = customer.order_set.all() if customer else []
-    context = {"orders": orders}
+    # customer = getattr(request.user, "customer", None)
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+    context = {
+        "orders": orders,
+        "total_orders": total_orders,
+        "delivered": delivered,
+        "pending": pending,
+    }
     return render(request, "accounts/userpage.html", context)
 
 
