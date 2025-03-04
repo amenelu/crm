b3e51817ef0b9598b5503cf4d8be8272c9222292
amen <fikruamen11@gmail.com>
2025-03-04 13:16:57 +0300

    constraint error resolved

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,9 +1,10 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 from .models import Customer
 
 
-def customer_profile(sender, instance, create, **kwargs):
+def customer_profile(sender, instance, created, **kwargs):
     if created:
         group = Group.objects.get(name="customer")
         instance.groups.add(group)
