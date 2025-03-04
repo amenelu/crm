fcc60fd1cf7a7e04d4d56adf0d5e713e528a20e1
amen <fikruamen11@gmail.com>
2025-03-05 00:44:29 +0300

    password reset path is set up

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("register/", views.registerPage, name="register"),
@@ -14,4 +14,5 @@ urlpatterns = [
     path("create_order/<str:pk>/", views.createOrder, name="create_order"),
     path("update_order/<int:pk>/", views.updateOrder, name="update_order"),
     path("delete_order/<str:pk>/", views.deleteOrder, name="delete_order"),
+    path("reset_password/", auth_views.PasswordResetView.as_view()),
 ]
