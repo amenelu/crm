9391f185df971d76df99d4d765c9b6f8f49fa2f2
amen <fikruamen11@gmail.com>
2025-03-05 22:07:25 +0300

    database path canged to postgres

--- crm/settings.py
+++ crm/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "crm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "DEMO_TEST",
+        "USER": "postgres",
+        "PASSWORD": "Project 1",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
