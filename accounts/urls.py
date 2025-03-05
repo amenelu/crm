753dfd1e25a685f8d04f6f6e806f584995949063
amen <fikruamen11@gmail.com>
2025-03-05 13:53:14 +0300

    path names added

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,5 +14,20 @@ urlpatterns = [
     path("create_order/<str:pk>/", views.createOrder, name="create_order"),
     path("update_order/<int:pk>/", views.updateOrder, name="update_order"),
     path("delete_order/<str:pk>/", views.deleteOrder, name="delete_order"),
-    path("reset_password/", auth_views.PasswordResetView.as_view()),
+    path("reset_password/", auth_views.PasswordResetView.as_view(), name="reset_view"),
+    path(
+        "reset_password_sent/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uid64>/<token>",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset_password_complete/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
